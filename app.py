af711a7da93b0b301b3eac3cf133a09db5deb7c6
pfistdo <pfistdo1@students.zhaw.ch>
2023-03-28 15:46:21 +0200

    Added gpt to flask backend

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_file, request
+import openai
 import os
 
 app = Flask(__name__, static_folder="web", static_url_path="/")
@@ -11,7 +12,28 @@ def hello_world():
 def ask_bot():
     if request.method == 'POST':
         input = request.form.get("query")
-    return "Bot response"
+        print(input)
+        response = chatbot(input)
+    return response
   
+def chatbot(input):
+
+
+    openai.Model.list()
+
+    messages = [{"role": "system", "content": "You are a helpful and kind AI assistant."}]
+    if input:
+        messages.append({"role": "user", "content": input})
+        chat = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "system", "content": reply})
+        print("Reply: ", reply)
+        return reply
+
 if __name__ == "__main__":
+    # Read the API key from a file
+    with open("api_key.txt", "r") as f:
+        openai.api_key = f.read()
     app.run()
--- web/js/script.js
+++ web/js/script.js
@@ -1,79 +1,30 @@
-function classify_image(event, text) {
-    var form_data = new FormData();
-    var img = document.getElementById("upload_image").files[0];
-    form_data.append("file", img);
-  
-    if (img != undefined) {
-      $.ajax({
-        url: "model/image", // point to server-side URL
-        dataType: "text", // what to expect back from server
-        cache: false,
-        contentType: false,
-        processData: false,
-        data: form_data,
-        type: "post",
-        success: function (response) {
-          alert(response, "success");
-          document.getElementById("classify_btn").disabled = false; // activate classify button
-        },
-        error: function (response) {
-          alert(response, "danger");
-        },
-      });
-    } else {
-    //   alert("Please select an image to upload!", "danger");
-    }
-}
+function ask_gpt() {
+  var question = document.getElementById("question").value;
+  var data = {
+    query: question
+  };
 
-// Upload image to server using AJAX Post
-function upload_image() {
-  var form_data = new FormData();
-  var ins = document.getElementById("upload_image").files[0];
-  form_data.append("file", ins);
-
-  if (ins != undefined) {
+  if (question != '') {
     $.ajax({
-      url: "upload", // point to server-side URL
+      url: "model/ask_gpt", // point to server-side URL
       dataType: "text", // what to expect back from server
-      cache: false,
-      contentType: false,
-      processData: false,
-      data: form_data,
+      data: data,
       type: "post",
       success: function (response) {
         alert(response, "success");
-        document.getElementById("classify_btn").disabled = false; // activate classify button
       },
       error: function (response) {
         alert(response, "danger");
       },
     });
   } else {
-    alert("Please select an image to upload!", "danger");
-  }
-}
-
-function preview_image() {
-  const preview = document.querySelector("#imagePreview");
-  const file = document.querySelector("input[type=file]").files[0];
-  const reader = new FileReader();
-
-  reader.addEventListener(
-    "load",
-    function () {
-      preview.innerHTML = '<img src="' + reader.result + '" class="img-fluid">';
-    },
-    false
-  );
-
-  if (file) {
-    reader.readAsDataURL(file);
+    //   alert("Please select an image to upload!", "danger");
   }
 }
 
 // Bootstrap alert
 const alert = (message, type) => {
-const alertPlaceholder = document.getElementById('alerts')
+  const alertPlaceholder = document.getElementById('alerts')
   const wrapper = document.createElement('div')
   wrapper.innerHTML = [
     `<div class="alert alert-${type} alert-dismissible fade show" role="alert">`,
