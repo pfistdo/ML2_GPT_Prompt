d0b4e907d7e125ff7b34bc4b9bf06ce903e90fe5
pfistdo <pfistdo1@students.zhaw.ch>
2023-03-28 15:34:39 +0200

    Revert "Added a new frontend"
    This reverts commit 0ece300bce7e2ea23a0f2d328e30526eeafbc588.

--- app.py
+++ app.py
@@ -1,39 +1,17 @@
-from flask import Flask, send_file, request, jsonify, render_template
-import openai
+from flask import Flask, send_file, request
+import os
 
-# Read the API key from a file
-with open("api_key.txt", "r") as f:
-    openai.api_key = f.read()
-
-# Set up the Flask app
 app = Flask(__name__, static_folder="web", static_url_path="/")
 
-# Set up the chatbot messages
-messages = [{"role": "system", "content": "You are a helpful and kind AI assistant."}]
-
-# Define the route to serve the HTML file
 @app.route("/")
-def home():
+def hello_world():
     return send_file("web/index.html")
 
-# Define the route to handle user input and return bot response
-@app.route("/ask_bot", methods=["POST"])
+@app.route("/model/ask_gpt", methods=["POST"])
 def ask_bot():
-    input_text = request.form.get("input_text")
-    
-    # Add the user message to the chat history
-    messages.append({"role": "user", "content": input_text})
-
-    # Generate a response from the chatbot
-    chat = openai.ChatCompletion.create(model="davinci", messages=messages)
-    reply = chat.choices[0].text
-
-    # Add the chatbot response to the chat history
-    messages.append({"role": "system", "content": reply})
-
-    # Return the chatbot response to the frontend
-    return jsonify({"response": reply})
-
-# Run the Flask app
+    if request.method == 'POST':
+        input = request.form.get("query")
+    return "Bot response"
+  
 if __name__ == "__main__":
     app.run()
