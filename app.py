0ece300bce7e2ea23a0f2d328e30526eeafbc588
FabioArdi <arditfab@students.zhaw.ch>
2023-03-28 15:24:49 +0200

    Added a new frontend

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-from flask import Flask, send_file, request
-import os
+from flask import Flask, send_file, request, jsonify, render_template
+import openai
 
+# Read the API key from a file
+with open("api_key.txt", "r") as f:
+    openai.api_key = f.read()
+
+# Set up the Flask app
 app = Flask(__name__, static_folder="web", static_url_path="/")
 
+# Set up the chatbot messages
+messages = [{"role": "system", "content": "You are a helpful and kind AI assistant."}]
+
+# Define the route to serve the HTML file
 @app.route("/")
-def hello_world():
+def home():
     return send_file("web/index.html")
 
-@app.route("/model/ask_gpt", methods=["POST"])
+# Define the route to handle user input and return bot response
+@app.route("/ask_bot", methods=["POST"])
 def ask_bot():
-    if request.method == 'POST':
-        input = request.form.get("query")
-    return "Bot response"
-  
+    input_text = request.form.get("input_text")
+    
+    # Add the user message to the chat history
+    messages.append({"role": "user", "content": input_text})
+
+    # Generate a response from the chatbot
+    chat = openai.ChatCompletion.create(model="davinci", messages=messages)
+    reply = chat.choices[0].text
+
+    # Add the chatbot response to the chat history
+    messages.append({"role": "system", "content": reply})
+
+    # Return the chatbot response to the frontend
+    return jsonify({"response": reply})
+
+# Run the Flask app
 if __name__ == "__main__":
     app.run()
